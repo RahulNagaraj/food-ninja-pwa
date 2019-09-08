17d85f27488124ce67c3a16f117fe653b3096921
Rahul N <rahul.nagarajmv@gmail.com>
2019-09-08 12:17:40 +0530

    update the ui

--- js/db.js
+++ js/db.js
@@ -4,7 +4,8 @@ db.collection('recipes').onSnapshot(snapshot => {
   snapshot.docChanges().forEach(change => {
     // console.log('change: ', change, change.doc.data(), change.doc.id);
     if (change.type === 'added') {
-      // Add document data to the list
+      // Add document data to the
+      renderRecipe(change.doc.data(), change.doc.id);
     } else if (change.type === 'removed') {
       // Remove the document from the list
     }
